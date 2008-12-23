2fb9227380e4dec7195cca5082ad762c7b53f246
Aaron Quint <aaron@quirkey.com>
2008-12-23 16:32:14 -0500

    Added default layout and CSS

--- rails_template.rb
+++ rails_template.rb
@@ -132,6 +132,31 @@ namespace :#{project_name} do
 end
 TEXT
 
+file 'app/views/layout/main.html.erb', <<-TEXT
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>#{project_name}</title>
+	
+	<%= javascript_include_tag :defaults %>
+	<%= stylesheet_link_tag '#{project_name}' %>
+</head>
+
+<body>
+  <%= yield =>
+</body>
+</html>
+TEXT
+
+file "public/stylesheets/#{project_name}.css", <<-TEXT
+/* #{project_name}.css / */
+
+TEXT
+
 puts "* Running database"
 rake 'db:create:all'
 rake 'db:sessions:create'
