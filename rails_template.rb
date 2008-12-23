1d0fd173315bbfe461374a65b24277163b47cea0
Aaron Quint <aaron@quirkey.com>
2008-12-23 11:41:57 -0500

    Moving around the order of operations

--- rails_template.rb
+++ rails_template.rb
@@ -9,15 +9,6 @@ git :init
 freeze!
 capify!
 
-in_root do
-  FileUtils.mkdir_p 'app/observers'
-  FileUtils.mkdir_p 'app/mailers'
-end
-
-environment "config.active_record.observers = :user_observer"
-environment "config.load_paths += %w[\#{Rails.root}/app/mailers \#{Rails.root}/app/observers]"
-
-
 puts "* Installing Plugins"
 plugin 'restful_authentication', :git => 'git://github.com/quirkey/restful-authentication.git', :submodule => true
 plugin 'flashdance', :git => 'git://github.com/quirkey/flashdance.git', :submodule => true
@@ -27,6 +18,21 @@ plugin 'jrails', :svn => 'http://ennerchi.googlecode.com/svn/trunk/plugins/jrail
 plugin 'halpers', :git => 'git://github.com/quirkey/halpers.git', :submodule => true
 
 git :submodule => 'update --init'
+
+puts "* Generating authentication"
+generate(:authenticated, 'user', 'sessions', '--include-activation', '--aasm', '--shoulda')
+
+
+in_root do
+  FileUtils.mkdir_p 'app/observers'
+  FileUtils.mkdir_p 'app/mailers'
+  FileUtils.mv 'app/models/user_mailer.rb', 'app/mailers/user_mailer.rb'
+  FileUtils.mv 'app/models/user_observer.rb', 'app/observers/user_observer.rb'
+end
+
+environment "config.active_record.observers = :user_observer"
+environment "config.load_paths += %w[\#{Rails.root}/app/mailers \#{Rails.root}/app/observers]"
+
 
 puts "* Adding gems"
 gem 'will_paginate'
@@ -39,16 +45,6 @@ gem 'rubyist-aasm', :source => 'http://gems.github.com', :lib => 'aasm'
 gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
 
 rake 'gems:install', :sudo => true
-
-puts "* Generating authentication"
-generate(:authenticated, 'user', 'sessions', '--include-activation', '--aasm', '--shoulda')
-
-in_root do
-  File.mv 'app/models/user_mailer.rb', 'app/mailers/user_mailer.rb'
-  File.mv 'app/models/user_observer.rb', 'app/observers/user_observer.rb'
-end
-
-
 
 puts "* Adding .gitignore"
 file '.gitignore', <<-TEXT
