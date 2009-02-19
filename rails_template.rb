5c7aab12923de3783abfa8d7ca9839cd4c89ed21
Aaron Quint <aaron@quirkey.com>
2009-02-19 17:01:33 -0500

    Fixing merge error in template

--- rails_template.rb
+++ rails_template.rb
@@ -114,11 +114,8 @@ namespace :#{project_name} do
 end
 TEXT
 
-<<<<<<< HEAD:rails_template.rb
-file 'app/views/layout/main.erb', <<-TEXT
-=======
+
 file 'app/views/layouts/main.html.erb', <<-TEXT
->>>>>>> 2ecaa46d96815196dd1bdf188fabb2946d62a2d0:rails_template.rb
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
